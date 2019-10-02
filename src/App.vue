b38fb7e187a3bd468c7bd827e1ef04245be4edaa
Vladislav Seletskiy <vselecky@gmail.com>
2019-10-02 10:27:37 +0300

    Extract navbar in separated component

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,44 @@
 <template>
-  <div id="app">
-    <nav class="navbar navbar-dark bg-dark navbar-expand-md">
-      <router-link to="/" class="navbar-brand">
-        <img src="./assets/images/logo.svg" class="d-block logo"  alt="Logo"/>
-      </router-link>
-      <div class="" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/irregular-verbs">Irregular verbs</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/nonprogressive-verbs">Non progressive verbs</router-link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <main>
-      <div class="container">
-        <router-view></router-view>
-      </div>
-    </main>
-  </div>
+    <div id="app">
+        <nav class="navbar navbar-dark bg-dark navbar-expand-md">
+            <router-link to="/" class="navbar-brand">
+                <img src="./assets/images/logo.svg" class="d-block logo" alt="Logo"/>
+            </router-link>
+            <div class="" id="navbarNav">
+                <Navbar v-bind:links="navbar_links"></Navbar>
+            </div>
+        </nav>
+        <main>
+            <div class="container">
+                <router-view></router-view>
+            </div>
+        </main>
+    </div>
 </template>
 
 <script>
 
-export default {
-  name: 'app',
-  components: {
-  }
-}
+    import Navbar from "./components/navbar/Navbar";
+    export default {
+        name: 'app',
+        data: function () {
+            return {
+                navbar_links: [
+                    {
+                        title: "Irregular verbs",
+                        to: "/irregular-verbs"
+                    },
+                    {
+                        title: "Non progressive verbs",
+                        to: "/nonprogressive-verbs"
+                    }
+                ]
+            }
+        },
+        components: {Navbar}
+    }
 </script>
 
 <style lang="scss">
-  @import "assets/styles";
+    @import "assets/styles";
 </style>
---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul class="navbar-nav">
+        <li class="nav-item" v-for="(link, index) in links" v-bind:key="index">
+            <router-link class="nav-link" v-bind:to="link.to" active-class="active">{{link.title}}</router-link>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "Navbar",
+        props: {
+            links: Array
+        }
+    }
+</script>
